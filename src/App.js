2526472b696a351773cfa1485fb5ccd22539f0d4
Dmitro Didenko <dmitrdida@ukr.net>
2018-08-14 15:19:02 +0300

    new branch didenko

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Main from './Components/Main/Main'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Main/>
+            </div>
+        );
+    }
 }
 
 export default App;
