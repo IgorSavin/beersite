705ed2f7175a78a8387480d42cc8ab4befe9f684
Alexandr Sokolov <sokolov1alexandr@gmail.com>
2018-08-15 18:09:03 +0300

    cw-done

--- src/react/actions/galleryActions.js
+++ src/react/actions/galleryActions.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 const galleryHandler =(arr)=>({type: 'GALLERY', payload: arr});
 
 const END_POINT = 'https://api.punkapi.com/v2/beers?';
-const getGallery=()=>axios.get(`${END_POINT}page=1&per_page=60`).then(data=>data.status === 200? data.data: null).catch(error=>console.log('error text: ',error));
+const getGallery=()=>axios.get(`${END_POINT}page=3&per_page=80`).then(data=>data.status === 200? data.data: null).catch(error=>console.log('error text: ',error));
 
 export const getGalleryAsync=()=> dispatch=>{
     getGallery().then(data=>dispatch(galleryHandler(data)));
 };
+
+export const getFilterBeer =(first,second)=> ({ type:"FILTER_BEER", firstParam:first, secondParam:second});
--- src/react/selectors/GallerySelector.js
+++
@@ -1,2 +0,0 @@
-export const gallery =state=>state.gallery;
-
