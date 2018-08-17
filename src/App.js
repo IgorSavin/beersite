dc48e022266fa423270cb8a5608779caa0a34a97
babenko-alexander <adr.babenko@gmail.com>
2018-08-17 12:49:51 +0300

    comments deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import BeerPage from './Components/BeerPage/BeerPage';
-// import FoodForm from './Components/FoodForm/FoodForm';
 import styles from './App.css';
 
 class App extends Component {
